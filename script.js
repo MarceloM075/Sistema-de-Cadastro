2f0137410b31208a0ce0a0c5a4ee2919b9b99704
Marcelo Marques Almeida <93165300+MarceloM075@users.noreply.github.com>
2021-12-15 03:58:29 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var nome = document.getElementById('nome')
+var cpf = document.getElementById('cpf')
+var telefone = document.getElementById('telefone')
+
+
+function mostrar(){
+    window.alert(`Os dados são ${nome.value}, ${cpf.value}, ${telefone.value}.`)
+}
