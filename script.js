d1ff028ed0204746bf1d059b1c908765fcdf6310
Marcelo Marques Almeida <93165300+MarceloM075@users.noreply.github.com>
2022-01-07 20:00:50 -0300

    confirmar em 'main'

--- script.js
+++ script.js
@@ -8,8 +8,9 @@ let senhaDigitada = document.getElementById("password")
 
 function entrarNoSistema(){
     if(usuarioPadrao.nome == usuarioDigitado.value && usuarioPadrao.senha == senhaDigitada.value){
-        window.location.assign('/cadastro.html')
         let token = Math.random().toString(16).substring(2)
+        
+        window.location.assign('/cadastro.html')
         localStorage.setItem('token', token)
     }else{
         const user = document.querySelector('#user')
